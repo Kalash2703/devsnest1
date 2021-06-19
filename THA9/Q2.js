4fd3a4dbf043b80566e7e5181c9e67dc2bfb5883
Kalash2703 <85036419+Kalash2703@users.noreply.github.com>
2021-06-19 22:25:40 +0530

    Add files via upload

---
+++ THA9/Q2.js
@@ -0,0 +1,8 @@
+function myFunc1(){
+    document.getElementById("1").innerHTML="Hello";
+}
+function myFunc2(){
+    var x = document.getElementById("2").value;
+    document.getElementById("3").innerHTML=
+    "You have written " + x + " till now!";
+}
