d258cda533f7ec201cb71878b267101f6e24d47f
Kalash2703 <85036419+Kalash2703@users.noreply.github.com>
2021-08-10 11:43:24 +0530

    Add files via upload

---
+++ THA 28/App.jsx
@@ -0,0 +1,62 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import {useState,useEffect} from 'react';
+export default function App() {
+  const [place,setPlace] = useState("");
+  const [placeData , setPlaceData] = useState({});
+  const updatePlaceData = () => {
+    fetch(`https://api.weatherapi.com/v1/current.json?key=3a81ceec694b4589a6791432212707&q=${place}`)
+    .then(res => res.json())
+    .then(data => {
+      setPlaceData(data);
+    });
+  };
+ 
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 form">
+            <input type="text" value={place} onChange={(e) => {
+              setPlace(e.target.value);
+            }} />
+            <button className="btn btn-primary" onClick={updatePlaceData}>Submit</button>
+            
+          </div>
+          <div className=' offset-md-4 col-12 col-md-4 weather'>
+            <div className="card">
+              
+             {placeData.location ?(
+               
+               <div>
+                <img src={placeData.current.condition.icon} />
+                <div className='temp'>{placeData.current.temp_c}°</div> 
+                <div className='desc'>{placeData.current.condition.text}</div>
+               <div className='place'>{placeData.location.name}</div>
+               <div className="container">
+                 <div className="row whp">
+                   <div className="col">
+                     <div className="title">Wind now</div>
+                     <div className="data">{placeData.current.wind_kph}<span className="unit">KM</span></div>
+                   </div>
+                   <div className="col">
+                   <div className="title">Humidity</div>
+                     <div className="data">{placeData.current.humidity}<span className="unit">%</span></div>
+                   </div>
+                   <div className="col">
+                   <div className="title">percipitation</div>
+                      <div className="data">{placeData.current.precip_in}<span className="unit">%</span></div>
+                   </div>
+                   
+                  
+                 </div>
+               </div>
+               </div>
+             ):<h2>Place Not Found</h2>}
+            </div>
+          </div>  
+        </div> 
+      </div>   
+    </div>
+  );
+}
